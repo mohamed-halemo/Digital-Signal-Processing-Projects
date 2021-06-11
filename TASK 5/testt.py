79eb0d272432fabc856a0612de412890765587c9
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2021-06-11 16:42:57 +0200

    interface DONEE

--- TASK 5/testt.py
+++ TASK 5/testt.py
@@ -2,20 +2,21 @@ from bokeh.models.markers import X
 from bokeh.plotting import figure, output_file, show, Column
 from bokeh.io import curdoc
 from bokeh.models import DataTable, TableColumn, PointDrawTool, ColumnDataSource, renderers,Button, CustomJS,Button
-
+from bokeh.layouts import column
 # output to static HTML file
 output_file("templates\Main.html")
 
 plot = figure(plot_width=300, plot_height=300,x_range=(-1.5, 1.5), y_range=(-1.5, 1.5))
 plot.circle(x=[0], y=[0], color="grey",
               radius=1,alpha=0.3 )
+renderer_2=plot.dot(x=[],y=[],size=30, color="red")
+renderer = plot.asterisk(x=[], y=[], size=10, color="blue")
+draw_tool = PointDrawTool(renderers=[renderer_2], empty_value='black')
+draw_tool_2 = PointDrawTool(renderers=[renderer], empty_value='black')
 
-renderer = plot.asterisk(x=[0, 0.1], y=[0, 0.1], size=10, color="blue")
-renderer_2=plot.dot(x=[0.2,0.5],y=[0.2,0.5],size=30, color="red")
-draw_tool = PointDrawTool(renderers=[renderer,renderer_2], empty_value='black')
-plot.add_tools(draw_tool)
+plot.add_tools(draw_tool,draw_tool_2)
 plot.toolbar.active_tap = draw_tool
-show(plot)
+show(column(plot))
 
 
 
