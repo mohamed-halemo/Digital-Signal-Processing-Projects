ff0400260b34a1f3cb5837a8c730f42e08691953
FathyJr10 <55717721+FathyJr10@users.noreply.github.com>
2021-06-13 02:36:43 +0200

    Magnitude Graph

--- TASK_5/testt.py
+++ TASK_5/testt.py
@@ -1,8 +1,12 @@
 from bokeh.layouts import row,column
+from bokeh.models.layouts import Row
 from bokeh.palettes import RdYlBu3
 from bokeh.plotting import figure, output_file, show, Column,curdoc
 from bokeh.models import DataTable, TableColumn, PointDrawTool, ColumnDataSource,CDSView, IndexFilter
 from math import *
+from scipy.signal import freqz
+import numpy as np
+from scipy.signal import zpk2ss, ss2zpk, tf2zpk, zpk2tf
 from cmath import *
 
 """ To Run the file ( python -m bokeh serve --show testt.py) in the terminal   """
@@ -10,32 +14,78 @@ from cmath import *
 s1 = figure(plot_width=300, plot_height=300,x_range=(-1.5, 1.5), y_range=(-1.5, 1.5),toolbar_location="below")
 s1.circle(x=[0], y=[0], color="grey",
               radius=1,alpha=0.3 )
+MagGraph=figure(x_range=(0,3.14), y_range=(0,10), tools=[],
+title='Magnitude',plot_width=400, plot_height=400)
+phaseGraph=figure(x_range=(0,3.14), y_range=(0,10), tools=[],
+title='Phase',plot_width=400, plot_height=400)
 #######################ZERO###############################################
-source = ColumnDataSource(data=dict(x_of_poles=[0.5], y_of_poles=[0.5]))
+source = ColumnDataSource(data=dict(x_of_poles=[], y_of_poles=[]))
 
-renderer = s1.asterisk(x="x_of_poles", y="y_of_poles", source=source, color='red', size=10)
+renderer = s1.circle(x="x_of_poles", y="y_of_poles", source=source, color='red', size=10)
 columns = [TableColumn(field="x_of_poles", title="x_of_poles"),
            TableColumn(field="y_of_poles", title="y_of_poles")
            ]
 table = DataTable(source=source, columns=columns, editable=True, height=200)
 ############################poles#########################################
-source_2 = ColumnDataSource(data=dict(x_of_zeros=[0], y_of_zeros=[0]))
+source_2 = ColumnDataSource(data=dict(x_of_zeros=[], y_of_zeros=[]))
 
-renderer_2 = s1.circle(x='x_of_zeros', y='y_of_zeros', source=source_2, color='blue', size=10)
+renderer_2 = s1.asterisk(x='x_of_zeros', y='y_of_zeros', source=source_2, color='blue', size=10)
 columns_2 = [TableColumn(field="x_of_zeros", title="x_of_zeros"),
            TableColumn(field="y_of_zeros", title="y_of_zeros")
            ]
 table_2 = DataTable(source=source_2, columns=columns_2, editable=True, height=200)
 ##########################################################################
+source_3= ColumnDataSource({
+    'h':[], 'w':[]
+})
+MagGraph.line(x='h',y='w',source=source_3)
+def update(attr, old, new):
+    ZeorsAndPoles()
+source.on_change('data',update)
+source_2.on_change('data',update)
+global Zero,Pole
+Zero = []
+Pole = []
+def ZeorsAndPoles():
+    
+    for i in range(len(source_2.data['x_of_zeros'])):
+        Zero.append(source_2.data['x_of_zeros'][i]+source_2.data['y_of_zeros'][i]*1j)
+    for i in range(len(source.data['x_of_poles'])):
+        Pole.append(source.data['x_of_poles'][i]+source.data['y_of_poles'][i]*1j)
+    
+    Mag()
+    
+def Mag():
+    source_3.data={
+    'h': [], 'w': []
+    }
+   
+    num, den=zpk2tf(Zero,Pole,1)
+    w,h=freqz(num,den,worN=10000)
+    mag=np.sqrt(h.real**2+h.imag**2)
+    phase=np.arctan(h.imag/h.real)
+    """ if len(source_2.data['x_of_zeros']) and len(source.data['x_of_poles']) ==0:
+        mag=[]
+        w=[]
+        phase=[]
+        source_3.data={'w': [], 'h': [] }
+ """
+    source_3.stream({
+    'h': w, 'w': mag
+    })
+   
+
+###########################################################################3
 draw_tool = PointDrawTool(renderers=[renderer], empty_value='red')
 draw_tool_2 = PointDrawTool(renderers=[renderer_2], empty_value='blue')
-pole=[]
-zero=[]
+
 
 s1.add_tools(draw_tool,draw_tool_2)
 s1.toolbar.active_tap = draw_tool
-s2 = figure(width=300, height=300, title=None)
-s2.toolbar.logo = None
-s2.toolbar_location = None
-plot=Column(s1,s2)
-curdoc().add_root(row(plot,table,table_2))
+MagGraph.toolbar.logo = None
+MagGraph.toolbar_location = None
+phaseGraph.toolbar.logo = None
+phaseGraph.toolbar_location = None
+plot2=Row(MagGraph,phaseGraph)
+plot=Row(s1,table,table_2)
+curdoc().add_root(column(plot,plot2))
--- TASK_5/trying.py
+++ TASK_5/trying.py
@@ -13,10 +13,16 @@ import numpy as np
 s1 = figure(plot_width=300, plot_height=300,x_range=(-1.5, 1.5), y_range=(-1.5, 1.5),toolbar_location="below")
 s1.circle(x=[0], y=[0], color="grey",
               radius=1,alpha=0.3 )
+
+MagGraph.line(x='w',y='h',source=source2)
 #######################ZERO###############################################
 source = ColumnDataSource({
-   'x': [], 'y': [], 'marker': []
+   'x_zeros': [], 'y_zeros': [], 
 })
+source_2 = ColumnDataSource({
+   'x_poles': [], 'y_poles': [], 
+})
+
 MagGraph=figure(x_range=(0,3.14), y_range=(0,10), tools=[],
            title='Magnitude',plot_width=400, plot_height=400)
 source2= ColumnDataSource({
@@ -28,20 +34,20 @@ y1=[0.25,0.5,0.2,0.75]
 x2=[1.25,-1.25,2,-2]
 y2=[1.25,-1.25,2,-2]
 
-renderer = s1.scatter(x='x', y='y',marker='circle', source=source,size=15)
-renderer_2 = s1.asterisk(x='x', y='y', source=source,marker='asterisk', color='color', size=10)
+renderer = s1.scatter(x='x_zeros', y='y_zeros', source=source,size=15)
+renderer_2 = s1.asterisk(x='x_poles', y='y_poles', source=source_2, size=10)
 
 MagGraph.line(x='w',y='h',source=source2)
 
-columns = [TableColumn(field="x", title="x"),
-           TableColumn(field="y", title="y")
+columns = [TableColumn(field="x_zeros", title="x_zeros"),
+           TableColumn(field="y_zeros", title="y_zeros")
            ]
 table = DataTable(source=source, columns=columns, editable=True, height=200)
 
-columns_2 = [TableColumn(field="x", title="x"),
-           TableColumn(field="y", title="y")
+columns_2 = [TableColumn(field="x_poles", title="x_poles"),
+           TableColumn(field="y_poles", title="y_poles")
            ]
-table_2 = DataTable(source=source2, columns=columns_2, editable=True, height=200)
+table_2 = DataTable(source=source_2, columns=columns_2, editable=True, height=200)
 def update(attr, old, new):
     ZeorsAndPoles()
 source.on_change('data',update)
@@ -51,11 +57,12 @@ Zero = []
 Pole = []
 def ZeorsAndPoles():
     
-    for i in range(len(source.data['x'])):
-        if source.data and source.data['marker'][i]=='circle':
-            Zero.append(source.data['x'][i]+source.data['y'][i]*1j)
-        elif source.data:
-            Pole.append(source.data['x'][i]+source.data['y'][i]*1j)
+    for i in range(len(source.data['x_zeros'])):
+        
+            Zero.append(source.data['x_zeros'][i]+source.data['y_zeros'][i]*1j)
+        
+            Pole.append(source_2.data['x_poles'][i]+source_2.data['y_poles'][i]*1j)
+    print("entered")
     Mag()
     
 def Mag():
@@ -67,7 +74,7 @@ def Mag():
     w,h=freqz(num,den,worN=10000)
     mag=np.sqrt(h.real**2+h.imag**2)
     phase=np.arctan(h.imag/h.real)
-    if len(source.data['x'])==0:
+    if len(source.data['x_zeros']) & len(source_2.data['x_poles']) ==0:
         mag=[]
         w=[]
         phase=[]
